d6033d5b9cc16366a31a1acf0bd6fdd4a1c86be1
Mihai Bivol <mm.bivol@gmail.com>
2016-02-03 15:42:09 +0200

    Remove all lfw references (almost)

--- train.lua
+++ train.lua
@@ -7,7 +7,7 @@
 
 require 'hdf5'
 require 'nngraph'
-require 'cudnn'
+require 'nn'
 require 'torch'
 require 'nn'
 require 'cunn'
@@ -17,13 +17,13 @@ require 'pl'
 require 'paths'
 ok, disp = pcall(require, 'display')
 if not ok then print('display not found. unable to plot') end
-adversarial = require 'lfw_adverserial'
+adversarial = require 'adverserial'
 
 
 ----------------------------------------------------------------------
 -- parse command-line options
 opt = lapp[[
-  -s,--save          (default "logs512_lfw64")      subdirectory to save logs
+  -s,--save          (default "logs")      subdirectory to save logs
   --saveFreq         (default 1)          save every saveFreq epochs
   -n,--network       (default "")          reload pretrained network
   -p,--plot                                plot while training
@@ -68,42 +68,55 @@ if opt.network == '' then
   ----------------------------------------------------------------------
   -- define D network to train
   model_D = nn.Sequential()
-  model_D:add(cudnn.SpatialConvolution(3, 32, 5, 5, 1, 1, 2, 2))
-  model_D:add(cudnn.SpatialMaxPooling(2,2))
-  model_D:add(cudnn.ReLU(true))
+  model_D:add(nn.SpatialConvolution(3, 16, 5, 5, 1, 1, 2, 2))
+  model_D:add(nn.SpatialMaxPooling(2,2))
+  model_D:add(nn.ReLU(true))
   model_D:add(nn.SpatialDropout(0.2))
-  model_D:add(cudnn.SpatialConvolution(32, 64, 5, 5, 1, 1, 2, 2))
-  model_D:add(cudnn.SpatialMaxPooling(2,2))
-  model_D:add(cudnn.ReLU(true))
+  model_D:add(nn.SpatialConvolution(16, 32, 5, 5, 1, 1, 2, 2))
+  model_D:add(nn.SpatialMaxPooling(2,2))
+  model_D:add(nn.ReLU(true))
   model_D:add(nn.SpatialDropout(0.2))
-  model_D:add(cudnn.SpatialConvolution(64, 96, 5, 5, 1, 1, 2, 2))
-  model_D:add(cudnn.ReLU(true))
-  model_D:add(cudnn.SpatialMaxPooling(2,2))
+  model_D:add(nn.SpatialConvolution(32, 32, 5, 5, 1, 1, 2, 2))
+  model_D:add(nn.ReLU(true))
+  model_D:add(nn.SpatialMaxPooling(2,2))
   model_D:add(nn.SpatialDropout(0.2))
-  model_D:add(nn.Reshape(8*8*96))
-  model_D:add(nn.Linear(8*8*96, 1024))
-  model_D:add(cudnn.ReLU(true))
+  model_D:add(nn.SpatialConvolution(32, 16, 5, 5, 1, 1, 2, 2))
+  model_D:add(nn.ReLU(true))
+  model_D:add(nn.SpatialMaxPooling(2,2))
+  model_D:add(nn.SpatialDropout(0.2))
+  model_D:add(nn.Reshape(4*4*16))
+  model_D:add(nn.Linear(4*4*16, 256))
+  model_D:add(nn.ReLU(true))
   model_D:add(nn.Dropout())
-  model_D:add(nn.Linear(1024,1))
+  model_D:add(nn.Linear(256,1))
   model_D:add(nn.Sigmoid())
 
   x_input = nn.Identity()()
-  lg = nn.Linear(opt.noiseDim, 128*8*8)(x_input)
-  lg = nn.Reshape(128, 8, 8)(lg)
-  lg = cudnn.ReLU(true)(lg)
+  lg = nn.Linear(opt.noiseDim, 16*4*4)(x_input)
+  lg = nn.Reshape(16, 4, 4)(lg)
+  lg = nn.ReLU(true)(lg)
+
   lg = nn.SpatialUpSamplingNearest(2)(lg)
-  lg = cudnn.SpatialConvolution(128, 256, 5, 5, 1, 1, 2, 2)(lg)
-  lg = nn.SpatialBatchNormalization(256)(lg)
-  lg = cudnn.ReLU(true)(lg)
+  lg = nn.SpatialConvolution(16, 32, 5, 5, 1, 1, 2, 2)(lg)
+  lg = nn.SpatialBatchNormalization(32)(lg)
+  lg = nn.ReLU(true)(lg)
+
   lg = nn.SpatialUpSamplingNearest(2)(lg)
-  lg = cudnn.SpatialConvolution(256, 256, 5, 5, 1, 1, 2, 2)(lg)
-  lg = nn.SpatialBatchNormalization(256)(lg)
-  lg = cudnn.ReLU(true)(lg)
+  lg = nn.SpatialConvolution(32, 64, 5, 5, 1, 1, 2, 2)(lg)
+  lg = nn.SpatialBatchNormalization(64)(lg)
+  lg = nn.ReLU(true)(lg)
+
   lg = nn.SpatialUpSamplingNearest(2)(lg)
-  lg = cudnn.SpatialConvolution(256, 128, 5, 5, 1, 1, 2, 2)(lg)
+  lg = nn.SpatialConvolution(64, 64, 5, 5, 1, 1, 2, 2)(lg)
+  lg = nn.SpatialBatchNormalization(64)(lg)
+  lg = nn.ReLU(true)(lg)
+
+  lg = nn.SpatialUpSamplingNearest(2)(lg)
+  lg = nn.SpatialConvolution(64, 32, 5, 5, 1, 1, 2, 2)(lg)
   lg = nn.SpatialBatchNormalization(128)(lg)
-  lg = cudnn.ReLU(true)(lg)
-  lg = cudnn.SpatialConvolution(128, 3, 3, 3, 1, 1, 1, 1)(lg)
+  lg = nn.ReLU(true)(lg)
+
+  lg = nn.SpatialConvolution(32, 3, 3, 3, 1, 1, 1, 1)(lg)
   model_G = nn.gModule({x_input}, {lg})
 
 else
@@ -127,13 +140,13 @@ print('Generator network:')
 print(model_G)
 
 
-local lfwHd5 = hdf5.open('datasets/lfw.hdf5', 'r')
-local data = lfwHd5:read('lfw'):all()
+local cahHd5 = hdf5.open('datasets/cah.hdf5', 'r')
+local data = cahHd5:read('lfw'):all() --old habits die hard
 data:mul(2):add(-1)
-lfwHd5:close()
+cahHd5:close()
 
 
-ntrain = 13000
+ntrain = 400--13000
 nval = 233
 trainData = data[{{1, ntrain}}]
 valData = data[{{ntrain, nval+ntrain}}]
@@ -170,6 +183,7 @@ sgdState_G = {
 
 -- Get examples to plot
 function getSamples(dataset, N)
+  print(N)
   local numperclass = numperclass or 10
   local N = N or 8
   local noise_inputs = torch.Tensor(N, opt.noiseDim)
@@ -189,7 +203,7 @@ end
 
 -- training loop
 while true do
-  local to_plot = getSamples(valData, 100)
+  local to_plot = getSamples(valData, 20)
   torch.setdefaulttensortype('torch.FloatTensor')
 
   trainLogger:style{['% mean class accuracy (train set)'] = '-'}
@@ -199,7 +213,7 @@ while true do
 
   local formatted = image.toDisplayTensor({input=to_plot, nrow=10})
   formatted:float()
-  image.save(opt.save .."/lfw_example_v1_"..(epoch or 0)..'.png', formatted)
+  image.save(opt.save .."/cah_example_v1_"..(epoch or 0)..'.png', formatted)
   if opt.gpu then
     torch.setdefaulttensortype('torch.CudaTensor')
   else
